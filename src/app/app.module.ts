5b56fde06a9ff5d7a6677b3b99732d8becb408c2
Anthony Ugo <anthonyugo@gmail.com>
2020-02-26 15:45:17 -0600

    First Push

---
+++ src/app/app.module.ts
@@ -0,0 +1,30 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import { FormsModule }   from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { UsersComponent } from './users/users.component';
+import { UserViewComponent } from './user-view/user-view.component';
+import { UserCreateComponent } from './user-create/user-create.component';
+import { UserEditComponent } from './user-edit/user-edit.component';
+
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    UsersComponent,
+    UserViewComponent,
+    UserCreateComponent,
+    UserEditComponent
+  ],
+  imports: [
+    BrowserModule,
+    HttpClientModule,
+    AppRoutingModule,
+    FormsModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
